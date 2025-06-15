a818ee9c845edfdc94c544509c333b97c7792a63
lufamukr <lufamukr@gmail.com>
2025-06-15 23:10:48 +0200

    begin to instruct

--- empty-folder/file.sql
+++ empty-folder/file.sql
@@ -1,1 +1,21 @@
-select * from current_BD;
+--1. CREATE TABLE people(
+--   id serial PRIMARY KEY,
+--   name TEXT,           
+--   age INTEGER
+-- );
+
+-- 2. INSERT INTO people(name, age) values('Canelo Alvarez', 35), ('Caleb Plant', 32);
+
+-- 3. ALTER TABLE people RENAME TO ring_names
+
+SELECT * FROM ring_names
+
+CREATE OR REPLACE FUNCTION increment(x INTEGER)
+RETURNS INTEGER AS $$
+BEGIN
+  RETURN x + 1;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT increment(age) from people;
+
